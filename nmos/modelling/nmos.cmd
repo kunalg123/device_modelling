d333fed2214d8059e81a52f4669c31585f6d60e4
Kunal Ghosh <kunalpghosh@gmail.com>
2019-09-13 11:37:27 +0530

    Update nmos.cmd

--- nmos/modelling/nmos.cmd
+++ nmos/modelling/nmos.cmd
@@ -1,104 +1,103 @@
 AdvancedCalibration
-#Defining Initial 2D Grid and Simulation Domain
-line x location= 0.0      spacing= 1.0<nm>  tag=SiTop        
-line x location=50.0<nm>  spacing=10.0<nm>                    
-line x location= 0.5<um>  spacing=50.0<nm>                      
-line x location= 2.0<um>  spacing= 0.2<um>                       
-line x location= 4.0<um>  spacing= 0.4<um>                       
-line x location=10.0<um>  spacing= 2.0<um>  tag=SiBottom   
+#Defining an initial 1D grid
+line x location=0.0 spacing=1<nm> tag=SiTop
+line x location=50<nm> spacing=10<nm>
+line x location=0.5<um> spacing=50<nm>
+line x location=2.0<um> spacing=0.2<um>
+line x location=4.0<um> spacing=0.4<um>
+line x location=10.0<um> spacing=2.0<um> tag=SiBottom
 
-line y location=0.0       spacing=50.0<nm>  tag=Mid         
-line y location=0.40<um>  spacing=50.0<nm>  tag=Right       
+line y location=0.0 spacing=50.0<nm> tag=Mid
+line y location=0.40<um> spacing=50.0<nm> tag=Right
 
-region Silicon xlo=SiTop xhi=SiBottom ylo=Mid yhi=Right
+#Defining simulation domain and initializing simulation
+region silicon xlo=SiTop xhi=SiBottom ylo=Mid yhi=Right
 
-init concentration=1.0e+15<cm-3> field=Phosphorus
+#initialize simulation
+init concentration=1.0e+13<cm-3> field=Boron wafer.orient=100
 
-#Boron Implantations
-implant  Boron  dose=2.0e13<cm-2>  energy=200<keV> tilt=0 rotation=0  
-implant  Boron  dose=1.0e13<cm-2>  energy= 80<keV> tilt=0 rotation=0  
-implant  Boron  dose=2.0e12<cm-2>  energy= 25<keV> tilt=0 rotation=0
+#refinement in vertical direction
+refinebox clear
+refinebox min = 0 max = 50.0<nm> xrefine = {2.0<nm> 10.0<nm>}
+refinebox min = 50.0<nm> max = 2.0<um> xrefine = {10.0<nm> 0.1<um> 0.2<um>}
+refinebox min = 2.0<um> max = 10.0<um> xrefine = {0.2<um> 2.0<um>}
 
-#Growing Gate Oxide
-grid set.min.normal.size=1<nm> set.normal.growth.ratio.2d=1.5
+#interface refinement
+refinebox interface.materials = {Oxide Silicon}
 
-diffuse temperature=1050<C> time=10.0<s>   
+#SnMesh settings for automatic meshing in newly generated layers
+pdbSet Grid SnMesh min.normal.size 1.0e-3
+pdbSet Grid SnMesh normal.growth.ratio.2d 1.4
 
+#p-well, anti-punchthrough & Vt adjustment implants
+implant Boron dose=2.0e13<cm-2> energy=200<keV> tilt=0 rotation=0
+implant Boron dose=1.0e13<cm-2> energy=80<keV> tilt=0 rotation=0
+implant Boron dose=2.0e12<cm-2> energy=25<keV> tilt=0 rotation=0
+
+#p-well: RTA of channel implants
+diffuse temperature=1050<C> time=10.0<s>
+
+#Saving structure
+struct tdr=1_Pwell FullD; # p-well
+
+#Growing gate oxide
+diffuse temperature=850<C> time=10.0<min> O2
 select z=1
 layers
+struct tdr=2_Oxidation FullD; # GateOx
 
-#Defining Polysilicon Gate
-deposit material= {PolySilicon} type=anisotropic time=1 rate= {0.18}
+#Defining polysilicon gate
+deposit poly type=isotropic thickness=0.18<um>
 
-mask name=gate_mask left=-1 right=90<nm>
-etch material= {PolySilicon} type=anisotropic time=1 rate= {0.2} mask=gate_mask
-etch material= {Oxide}       type=anisotropic time=1 rate= {0.1}
+#Poly gate pattern/etch
+mgoals accuracy=2e-5
+mask name=gate_mask segments = {-1 90<nm>}
+etch poly type=anisotropic thickness=0.2<um> mask=gate_mask
+etch oxide type=anisotropic thickness=0.1<um>
+struct tdr=3_Polygate ; # Polygate
 
-#Polysilicon Reoxidation
-diffuse temperature=900<C> time=10.0<min>  O2 
-pdbSet Oxide Grid perp.add.dist 1e-7
+#Poly reoxidation
+diffuse temperature=900<C> time=10.0<min> O2 pressure=0.5<atm>
+struct tdr=4_Polyreox ; #Poly Reox
 
-#Saving Snapshots
-struct tdr=n1node1_NMOS1  ; # p-Well
+#LDD implantation
+refinebox silicon min = {0.0 0.045<um>} max = {0.1<um> 0.125<um>} xrefine= 0.01<um> yrefine=0.01<um>
+grid remesh
+implant Arsenic dose=4e14<cm-2> energy=10<keV> tilt=0 rotation=0
+diffuse temperature=1050<C> time=0.1<s>; # Quick activation
+struct tdr=5_LDD ; # LDD Implant
 
-#Remeshing for LDD and Halo Implantations
-refinebox Silicon min= {0.0 0.05} max= {0.1 0.12} xrefine= {0.01 0.01 0.01} \
-                                                  yrefine= {0.01 0.01 0.01} add
-refinebox remesh
+#Halo implantation: Quad HALO implants
+diffuse temperature=1050<C> time=5.0<s>
+struct tdr=6_Halo ; # Halo RTA
 
-#LDD and Halo Implantations
-implant Arsenic dose=4e14<cm-2> energy=10<keV> tilt=0 rotation=0  
+#Nitride spacer
+deposit nitride type=isotropic thickness=60<nm>
+etch nitride type=anisotropic thickness=84<nm> isotropic.overetch=0.01
+etch oxide type=anisotropic thickness=10<nm>
+struct tdr=7_Spacer  ; # Spacer
 
-implant Boron dose=0.25e13<cm-2> energy=20<keV> tilt=30<degree> \
-              rotation=0            
-implant Boron dose=0.25e13<cm-2> energy=20<keV> tilt=30<degree> \
-              rotation=90<degree>   
-implant Boron dose=0.25e13<cm-2> energy=20<keV> tilt=30<degree> \
-              rotation=180<degree>  
-implant Boron dose=0.25e13<cm-2> energy=20<keV> tilt=30<degree> \
-              rotation=270<degree>  
+#N+ implantation 
+refinebox silicon min= {0.04 0.11} max= {0.18 0.4} xrefine= 0.01<um> yrefine= {0.02<um> 0.05<um>}
+grid remesh
+implant Arsenic dose=5e15<cm-2> energy=40<keV> tilt=7<degree> rotation=-90<degree>
 
-diffuse temperature=1050<C> time=0.1<s> ; # Quick activation 
+#N+ implantation and final RTA
+diffuse temperature=1050<C> time=10.0<s>
+struct tdr=8_Source ; # S/D implants
 
-#Spacer Formation
-deposit material= {Nitride} type=isotropic   time=1 rate= {0.06}
-etch    material= {Nitride} type=anisotropic time=1 rate= {0.084} \
-				isotropic.overetch=0.01
-etch    material= {Oxide}   type=anisotropic time=1 rate= {0.01} 
+#Remove bottom of structure
+transform cut location=1.00 down
 
-#Remeshing for Source-Drain Implantations
-refinebox Silicon min= {0.04 0.12} max= {0.18 0.4} xrefine= {0.01 0.01 0.01} \
-                                                   yrefine= {0.05 0.05 0.05} add
-refinebox remesh
-#implant Arsenic dose=5e15<cm-2> energy=40<keV> 
-
-#Source-Drain Implantations
-implant Arsenic dose=5e15<cm-2> energy=40<keV> 
-diffuse temperature=1050<C> time=10.0<s> 
-
-#Contact Pads
-deposit material= {Aluminum} type=isotropic time=1 rate= {0.03}
-
-mask name=contacts_mask left=0.2<um> right=1.0<um>
-etch material= {Aluminum} type=anisotropic time=1 rate= {0.25} \
-     mask=contacts_mask
-
-
-#Saving the Full Structure
+#Reflect
 transform reflect left
+
+#Contact pads
 contact name="substrate" bottom Silicon
 contact name="source" box Silicon xlo=0.0 xhi=0.005 ylo=-0.4 yhi=-0.2
 contact name="drain" box Silicon xlo=0.0 xhi=0.005 ylo=0.2 yhi=0.4
 contact name="gate" box PolySilicon xlo=-0.181 xhi=-0.05 ylo=-0.088 yhi=0.088
- 
-struct tdr= n1node1_NMOS ; # Final
 
-#Extracting 1D Profiles
-SetPlxList   {BTotal NetActive}
-WritePlx n1node1_NMOS_channel.plx  y=0.0 Silicon
-SetPlxList   {AsTotal BTotal NetActive}
-WritePlx n1node1_NMOS_ldd.plx y=0.1 Silicon
-SetPlxList   {AsTotal BTotal NetActive}
-WritePlx n1node1_NMOS_sd.plx y=0.39 Silicon
-
+#Final
+struct smesh=nmos_final
 
